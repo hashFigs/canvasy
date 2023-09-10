bfd1a4a9c8117b481666b2818bc5e24ecd45fc8b
hashFigs <jordi@hashbrowns.dev>
2023-09-10 15:32:39 -0500

    with Accounts

--- lib/canvas_app_web/router.ex
+++ lib/canvas_app_web/router.ex
@@ -1,6 +1,8 @@
 defmodule CanvasAppWeb.Router do
   use CanvasAppWeb, :router
 
+  import CanvasAppWeb.AccountAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule CanvasAppWeb.Router do
     plug :put_root_layout, html: {CanvasAppWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_account
   end
 
   pipeline :api do
@@ -49,6 +52,44 @@ defmodule CanvasAppWeb.Router do
 
       live_dashboard "/dashboard", metrics: CanvasAppWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", CanvasAppWeb do
+    pipe_through [:browser, :redirect_if_account_is_authenticated]
+
+    live_session :redirect_if_account_is_authenticated,
+      on_mount: [{CanvasAppWeb.AccountAuth, :redirect_if_account_is_authenticated}] do
+      live "/accounts/register", AccountRegistrationLive, :new
+      live "/accounts/log_in", AccountLoginLive, :new
+      live "/accounts/reset_password", AccountForgotPasswordLive, :new
+      live "/accounts/reset_password/:token", AccountResetPasswordLive, :edit
+    end
+
+    post "/accounts/log_in", AccountSessionController, :create
+  end
+
+  scope "/", CanvasAppWeb do
+    pipe_through [:browser, :require_authenticated_account]
+
+    live_session :require_authenticated_account,
+      on_mount: [{CanvasAppWeb.AccountAuth, :ensure_authenticated}] do
+      live "/accounts/settings", AccountSettingsLive, :edit
+      live "/accounts/settings/confirm_email/:token", AccountSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", CanvasAppWeb do
+    pipe_through [:browser]
+
+    delete "/accounts/log_out", AccountSessionController, :delete
+
+    live_session :current_account,
+      on_mount: [{CanvasAppWeb.AccountAuth, :mount_current_account}] do
+      live "/accounts/confirm/:token", AccountConfirmationLive, :edit
+      live "/accounts/confirm", AccountConfirmationInstructionsLive, :new
     end
   end
 end
